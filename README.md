ec83f6acd2a010cf103300b1d356e56f69afe830
jlafforgue <jlafforgue@imjweb.fr>
2021-03-28 11:25:38 +0200

    Restrict access for uid

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Shaarli-SSO-SimpleSAMLphp
 
-This plugin allows to connect a user in Shaarli via SSO
+This plugin allows to connect a user in Shaarli via SSO and and authorizes him to connect only to his instance
 
 ## Requirements
 - Shaarli >= 0.12.1 
-- SimpleSAMLphp configured on the same server as a Service Provider
+- SimpleSAMLphp configured on the same server as a Service Provider (https://simplesamlphp.org/docs/stable/simplesamlphp-sp)
 
 ## Installation
 
@@ -13,3 +13,9 @@ Add the plugins in Shaarli, in the "plugins" folder
 Activate the plugin and include the path of simpleSAMLphp (SSO_SIMPLESAML_PATH)
 
 You can now authenticate and login in Shaarli via SSO
+
+## Operating mode
+- When I access my Shaarli instance I can connect to it.
+- By default, if I access another Shaarli instance and I am already logged in to mine, we are logged in to the other instance. I have set up an automatic logout.
+- When I am on the login page, the connection is refused if I am not on my instance
+- If I am connected to the SP and I am not on my Shaarli instance, the connection icon is hidden
--- sso_simplesaml.php
+++ sso_simplesaml.php
@@ -37,44 +37,85 @@ function sso_simplesaml_t($text, $nText = '', $nb = 1)
  */
 function sso_simplesaml_init($conf)
 {
-	 // Custom translation with the domain 'ssosimplesaml'
+	// Custom translation with the domain 'ssosimplesaml'
 	if (! $conf->exists('translation.extensions.sso_simplesaml')) {
         $conf->set('translation.extensions.sso_simplesaml', 'plugins/sso_simplesaml/languages/');
         $conf->write(true);
     }
 
-   $errors = [];
+	$errors = [];
+   
+	// If the user is connected with SimpleSAML, we check if he can be connected to this instance of Shaarli
+	if(!empty($_SESSION['sso_uid'])){
+	   // The user can only connect to the instance that contains his name with uid
+	   if(strpos($_SERVER['SCRIPT_NAME'], $_SESSION['sso_uid']) === false){
+		   if (isset($_SESSION['ip']) && isset($_SESSION['username'])) {
+				unset($_SESSION['ip']);
+				unset($_SESSION['expires_on']);
+				unset($_SESSION['username']);
+				unset($_SESSION['visibility']);
+				
+				// JS redirection to home directory
+				echo "<script>
+					window.location = '//".$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']."';
+				</script>";
+			}
+			echo '<style>#login-button{display:none;}</style>';
+		}
+	} 
 
-	//Check the inclusion of library simpleSAMLPhp
-	if(file_exists($conf->get('plugins.SSO_SIMPLESAML_PATH'))){
-		if(strpos( $_SERVER['SCRIPT_URL'], 'login') !== false){
+  
+	// On Login page
+	if(strpos( $_SERVER['SCRIPT_URL'], 'login') !== false){
+		
+		// Check inclusion of library simpleSAMLPhp and initialize the instance
+		if(file_exists($conf->get('plugins.SSO_SIMPLESAML_PATH'))){	
 			
-			// Authenticiation
 			require_once($conf->get('plugins.SSO_SIMPLESAML_PATH'));
 			$as = new \SimpleSAML\Auth\Simple('default-sp');
+			$attrs = $as->getAttributes();
+		   
+			$session = \SimpleSAML\Session::getSessionFromRequest();
+			$session->cleanup();
+			   
+			// Authenticiation with SimpleSAML
 			$as->requireAuth();
+			
 			$session = \SimpleSAML\Session::getSessionFromRequest();
 			$session->cleanup();
 			
-			// If authentification is OK, auto connection to Shaarli
-			$client_ip_id = client_ip_id($_SERVER);
-			$sessionManager = new SessionManager($_SESSION, $conf, session_save_path());
-			$sessionManager->initialize();
-			$sessionManager->storeLoginInfo($client_ip_id);
-			
-			// JS redirection to home directory
-			echo "<script>
-				window.location = '//".$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']."';
-			</script>";
-		}
+			$attrs = $as->getAttributes();
+			$_SESSION['sso_uid'] = $attrs['uid'][0];
 
-    } else {
-		$errors[] = sso_simplesaml_t('SimpleSAMLphp path is not defined or does not exist on the server');
-	}
+			// The user can only connect to the instance that contains his name with uid
+			if(strpos($_SERVER['SCRIPT_NAME'], $attrs['uid'][0]) === false){
+				
+				$sessionManager = new SessionManager($_SESSION, $conf, session_save_path());
+				$sessionManager->setSessionParameter(
+				SessionManager::KEY_ERROR_MESSAGES,
+					[t('Wrong login/password.')]
+				);
+			
+			} else {
+				// If authentification is OK, auto connection to Shaarli
+				$client_ip_id = client_ip_id($_SERVER);
+				$sessionManager = new SessionManager($_SESSION, $conf, session_save_path());
+				$sessionManager->initialize();
+				$sessionManager->storeLoginInfo($client_ip_id);
+				
+				// JS redirection to home directory
+				echo "<script>
+					window.location = '//".$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']."';
+				</script>";
+			}
+		}else {
+			$errors[] = sso_simplesaml_t('SimpleSAMLphp path is not defined or does not exist on the server');
+		}
+	} 
+	
     return $errors;
 	
 }
-
 
 
 /**
